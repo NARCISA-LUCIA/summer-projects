3d30da127ba807d8be558f896ba33c4aef8acb52
NARCISA-LUCIA <rusnarcisa16@gmail.com>
2022-06-22 21:42:32 +0300

    Create left-menu and page how-i-am-doing (#1)
    * Create left-menu and page how-i-am-doing
    
    * Add review changes

---
+++ src/app/how-i-am-doing/how-i-am-doing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-how-i-am-doing',
+  templateUrl: './how-i-am-doing.component.html',
+  styleUrls: ['./how-i-am-doing.component.css']
+})
+export class HowIAmDoingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
