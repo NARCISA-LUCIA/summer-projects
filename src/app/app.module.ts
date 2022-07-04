8da88be9d9c281d76aa59bbe864785ba9eb587ff
NARCISA-LUCIA <rusnarcisa16@gmail.com>
2022-07-04 13:19:00 +0300

    Add portofolio and education page (#2)
    * Add portofolio and education page
    * Added support for fix width
    * Fix width for all pages
    * Add review changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import { LeftMenuComponent } from './left-menu/left-menu.component';
 import { HowIAmDoingComponent } from './how-i-am-doing/how-i-am-doing.component';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { EducationComponent } from './education/education.component';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatCardModule } from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import { PortofolioComponent } from './portofolio/portofolio.component';
+import { PortofolioCardComponent } from './portofolio/portofolio-card/portofolio-card.component';
+import { EducationCardComponent } from './education/education-card/education-card.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LeftMenuComponent,
-    HowIAmDoingComponent
+    HowIAmDoingComponent,
+    EducationComponent,
+    PortofolioComponent,
+    PortofolioCardComponent,
+    EducationCardComponent
   ],
   imports: [
     BrowserModule,
@@ -28,6 +39,10 @@ import {MatProgressBarModule} from '@angular/material/progress-bar';
     MatFormFieldModule,
     MatInputModule,
     MatProgressBarModule,
+    MatProgressBarModule,
+    MatStepperModule,
+    MatCardModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
